0ec29017d762b3b893cf811ef6e804441be85466
Richtofen4 <dawid.ciurkot@interia.pl>
2024-04-08 23:09:02 +0200

    Initial design connection to the frontend

--- server.js
+++ server.js
@@ -15,7 +15,22 @@ app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get('/', (req, res) => {
         res.send('Hello, World!');
 })
+/*
+app.get('/register', (req, res) => {
+        res.send('This is the registration page.');
+})
+*/
 
-
+app.post('/register', async (req, res) => {
+        try {
+          const { nick, email, password } = req.body;
+          //Logika zapisu do bazy danych
+          //wait pool.query('INSERT INTO users (nick, email, password) VALUES ($1, $2, $3)', [nick, email, password]);
+      
+          res.status(201).json({ message: 'Użytkownik zarejestrowany' });
+        } catch (error) {
+          res.status(500).json({ message: 'Wystąpił błąd podczas rejestracji' });
+        }
+});
 
 app.listen(port, () => console.log(`Server has started on port: ${port}`))
