80bfba9a837f48850a7c2b67043609b170d38dd4
Przemek Kubas <kubasprzemek9@gmail.com>
2024-04-30 01:03:31 +0200

    Last one preparing to marge

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,64 +1,64 @@
 module.exports = (sequelize, Sequelize) => {
   const User = sequelize.define("users", {
-    username: {
+    ID_USER: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+  },
+  email: {
+      type: Sequelize.STRING,
+
+  },
+  password: {
+      type: Sequelize.STRING,
+  },
+  username: {
+      type: Sequelize.STRING,
+  },
+  about: {
+      type: Sequelize.TEXT
+  },
+  avatar: {
       type: Sequelize.STRING
-    },
-    email: {
+  },
+  country: {
       type: Sequelize.STRING
-    },
-    password: {
+  },
+  city: {
       type: Sequelize.STRING
-    },
-    status:{
+  },
+  contact: {
+      type: Sequelize.STRING
+  },
+  isAdmin: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+  },
+  isBanned: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+  },
+  status: {
       type: Sequelize.STRING,
       defaultValue: 'Created'
-    },
-    confirmationCode:{
+  },
+  confirmationCode: {
       type: Sequelize.STRING,
       unique: true
-    },
-    IsAdmin:{
-      type: Sequelize.STRING,
-      defaultValue: 'No',
-    },
-    country: {
-        type: Sequelize.STRING
-    },
-    city: {
-        type: Sequelize.STRING
-    },
-    contact: {
-        type: Sequelize.STRING
-    },
-    isAdmin: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false
-    },
-    isBanned: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false
-    },
-    status: {
-        type: Sequelize.STRING,
-        defaultValue: 'Created'
-    },
-    confirmationCode: {
-        type: Sequelize.STRING,
-        unique: true
-    },
-    ID_LANGUAGE: {
-        type: Sequelize.INTEGER,
-        allowNull: true,
-        references: {
-            model: 'languages',
-            key: 'ID_LANGUAGE'
-        }
-    }
-    },
-    {
-    timestamps: false,
-    freezeTableName: true
-    });
+  },
+  ID_LANGUAGE: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+          model: 'languages',
+          key: 'ID_LANGUAGE'
+      }
+  }
+  },
+  {
+  timestamps: false,
+  freezeTableName: true
+  });
 
   return User;
 };
