40c184faffa5e04931ae1e1c40c378ac84cdd894
sneark <99653016+sneark@users.noreply.github.com>
2024-05-13 23:34:56 +0200

    Revert "Hosting test"
    This reverts commit 83948c0a3484e7382a9a2796d9709ae4dff500e2.

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,34 +1,19 @@
-//hosting
+//docker setup
 module.exports = {
-  HOST: "pg-zagrajzemna-123.c.aivencloud.com",
-  USER: "avnadmin",
-  PASSWORD: "AVNS_xC2JvrBRqM3258Y3kq2",
-  DB: "defaultdb",
-  port: 28985,
+  HOST: "db",
+  USER: "postgres",
+  PASSWORD: "postgres",
+  DB: "postgres_db",
+  port: 5432,
   dialect: "postgres",
   pool: {
-    max: 5,
+    max: 100,
     min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+    acquire: 10000,
+    idle: 20000
+  },
+  logging: false
 };
-// //docker setup
-// module.exports = {
-//   HOST: "db",
-//   USER: "postgres",
-//   PASSWORD: "postgres",
-//   DB: "postgres_db",
-//   port: 5432,
-//   dialect: "postgres",
-//   pool: {
-//     max: 100,
-//     min: 0,
-//     acquire: 10000,
-//     idle: 20000
-//   },
-//   logging: false
-// };
 
 
 // // local computer
