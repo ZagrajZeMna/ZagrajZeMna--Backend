0aee14722cfafd7991d3b428350f8a14182dde9a
EKHO <lukasz.ksiazek1350@gmail.com>
2024-05-13 20:11:47 +0200

    Adding game from admin

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ require('./app/routes/user.routes')(app);
 require('./app/routes/lobby.routes')(app);
 require('./app/routes/profile.routes')(app);
 require('./app/routes/mainGame.routes')(app);
+require('./app/routes/admin.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 4000;
