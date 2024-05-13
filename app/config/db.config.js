83948c0a3484e7382a9a2796d9709ae4dff500e2
sneark <99653016+sneark@users.noreply.github.com>
2024-05-13 22:36:32 +0200

    Hosting test

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,19 +1,34 @@
-//docker setup
+//hosting
 module.exports = {
-  HOST: "db",
-  USER: "postgres",
-  PASSWORD: "postgres",
-  DB: "postgres_db",
-  port: 5432,
+  HOST: "pg-zagrajzemna-123.c.aivencloud.com",
+  USER: "avnadmin",
+  PASSWORD: "AVNS_xC2JvrBRqM3258Y3kq2",
+  DB: "defaultdb",
+  port: 28985,
   dialect: "postgres",
   pool: {
-    max: 100,
+    max: 5,
     min: 0,
-    acquire: 10000,
-    idle: 20000
-  },
-  logging: false
+    acquire: 30000,
+    idle: 10000
+  }
 };
+// //docker setup
+// module.exports = {
+//   HOST: "db",
+//   USER: "postgres",
+//   PASSWORD: "postgres",
+//   DB: "postgres_db",
+//   port: 5432,
+//   dialect: "postgres",
+//   pool: {
+//     max: 100,
+//     min: 0,
+//     acquire: 10000,
+//     idle: 20000
+//   },
+//   logging: false
+// };
 
 
 // // local computer
